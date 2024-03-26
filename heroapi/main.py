9366404d3a3b3548e3b759b8a74ff734dfa6219b
Tehseen Afzal <abuashja@gmail.com>
2024-03-26 23:34:09 +0500

    Update main.py
    Update models validations in routes and create get single hero by id route

--- heroapi/main.py
+++ heroapi/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import  Session, SQLModel, create_engine, select
 from .models import * 
 
-DATABASE_URL = ""
+DATABASE_URL = "postgresql://abuashja:JLR0rj8nOGCE@ep-purple-bonus-a5mp6spb.us-east-2.aws.neon.tech/todos?sslmode=require"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
@@ -15,6 +15,10 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
+@app.get("/")
+def welcome_fast():
+    return {"message":"Welcome to FastAPI SQLModel"}
+
 @app.post("/heroes/", response_model=HeroRead)
 def create_hero(hero: HeroCreate):
     with Session(engine) as session:
@@ -24,8 +28,16 @@ def create_hero(hero: HeroCreate):
         session.refresh(db_hero)
         return db_hero
 
-@app.get("/heroes/", response_model=list[Hero])
+@app.get("/heroes/", response_model=list[HeroRead])
 def read_heroes():
     with Session(engine) as session:
         heroes = session.exec(select(Hero)).all()
         return heroes
+    
+@app.get("/heroes/{hero_id}", response_model=HeroRead)
+def read_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+        return hero
